196a8615148723b85714a749ed0793656c57e65c
shegang <2904344804@qq.com>
2016-02-19 16:56:45 +0800

    commit

---
+++ app/src/main/java/org/sunger/net/presenter/LoginPresenter.java
@@ -0,0 +1,13 @@
+package org.sunger.net.presenter;
+
+/**
+ * Created by Administrator on 2015/11/17.
+ */
+
+/**
+ * @登陆的视图和数据控制
+ */
+public interface LoginPresenter {
+
+    void login(String phone, String pwd);
+}
